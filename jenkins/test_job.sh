5b355a175bea148d780d44b4b20c7f41925c650d
harunkarateketurkiye <harunkarateketurkiye@gmail.com>
2023-12-18 10:53:43 +0000

    helm chart was created

--- jenkins/test_job.sh
+++ jenkins/test_job.sh
@@ -15,3 +15,45 @@ aws ecr create-repository \
 . ./jenkins/prepare_tags.sh
 . ./jenkins/ecr_build_images.sh
 . ./jenkins/push_images.sh
+
+sonra terminalde kurulacak
+
+aws s3api create-bucket \
+    --bucket harun-helm-repo \
+    --region us-east-1
+
+aws s3api put-object --bucket harun-helm-repo --key stable/myapp/
+
+helm plugin install https://github.com/hypnoglow/helm-s3.git
+
+aynısını sudo su - jenkins için de kurmalısın
+
+helm s3 init s3://harun-helm-repo/stable/myapp/
+
+helm create petclinic-chart
+
+helm repo add stable-myapp s3://harun-helm-repo/stable/myapp/
+
+helm repo ls
+
+helm package petclinic-chart
+
+helm s3 push ./petclinic-chart-0.1.0.tgz stable-myapp
+
+helm search repo -l
+
+helm repo update
+
+helm upgrade --install my-app-release stable-myapp/my-app --version 0.1.1 --namespace dev
+
+
+
+
+
+
+
+
+
+
+
+envsubst < k8s/petclinic-chart/values_template.yaml > k8s/petclinic-chart/values.yaml
