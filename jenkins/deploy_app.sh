e6d26c18a0f29e83a856c5b8fe922853f3e6b712
harunkarateketurkiye <harunkarateketurkiye@gmail.com>
2023-12-19 10:03:57 +0000

    Jenkinsfile was created

---
+++ jenkins/deploy_app.sh
@@ -0,0 +1,13 @@
+envsubst < k8s/petclinic-chart/values_template.yaml > k8s/petclinic-chart/values.yaml
+sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic-chart/Chart.yaml
+AWS_REGION=${AWS_REGION} helm repo add stable-myapp s3://harun-helm-repo/stable/myapp/ || echo 'Helm repo is already exist'
+AWS_REGION=${AWS_REGION} helm repo update
+helm package petclinic-chart
+AWS_REGION=${AWS_REGION} helm s3 push ./petclinic-chart-${BUILD_NUMBER}.tgz stable-myapp
+kubectl create ns petclinic || echo 'namespace is already exist'
+kubectl delete secret regcred --namespace petclinic || \
+kubectl create secret generic regcred --namespace petclinic \
+    --from-file=.dockerconfigjson=var/lib/jenkins/.docker/config.json \
+    --type=kubernetes.io/dockerconfigjson
+AWS_REGION=${AWS_REGION} helm repo update
+AWS_REGION=${AWS_REGION} helm upgrade --install my-app-release stable-myapp/petclinic-chart --version ${BUILD_NUMBER} --namespace petclinic
