9f1252cf1b7a8dc52d06a63613a0be28ccb9d2f1
harunkarateketurkiye <harunkarateketurkiye@gmail.com>
2023-12-21 09:54:38 +0000

    completed project

--- jenkins/deploy_app.sh
+++ jenkins/deploy_app.sh
@@ -3,7 +3,7 @@ sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic-chart/Chart.yaml
 AWS_REGION=${AWS_REGION} helm repo add stable-myapp s3://harun-helm-repo/stable/myapp/ || echo 'Helm repo is already exist'
 AWS_REGION=${AWS_REGION} helm repo update
 helm package k8s/petclinic-chart
-AWS_REGION=${AWS_REGION} helm s3 push ./petclinic-chart-${BUILD_NUMBER}.tgz stable-myapp
+AWS_REGION=${AWS_REGION} helm s3 push --force ./petclinic-chart-${BUILD_NUMBER}.tgz stable-myapp
 kubectl create ns petclinic || echo 'namespace is already exist'
 kubectl delete secret regcred --namespace petclinic || \
 kubectl create secret generic regcred --namespace petclinic \
