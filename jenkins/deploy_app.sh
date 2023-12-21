8bee600516fa5c4e4a712dbbfd99d0748cdafcdd
harunkarateketurkiye <harunkarateketurkiye@gmail.com>
2023-12-21 09:53:38 +0000

    changed

--- jenkins/deploy_app.sh
+++ jenkins/deploy_app.sh
@@ -2,7 +2,7 @@ envsubst < k8s/petclinic-chart/values_template.yaml > k8s/petclinic-chart/values
 sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic-chart/Chart.yaml
 AWS_REGION=${AWS_REGION} helm repo add stable-myapp s3://harun-helm-repo/stable/myapp/ || echo 'Helm repo is already exist'
 AWS_REGION=${AWS_REGION} helm repo update
-helm package petclinic-chart
+helm package k8s/petclinic-chart
 AWS_REGION=${AWS_REGION} helm s3 push ./petclinic-chart-${BUILD_NUMBER}.tgz stable-myapp
 kubectl create ns petclinic || echo 'namespace is already exist'
 kubectl delete secret regcred --namespace petclinic || \
